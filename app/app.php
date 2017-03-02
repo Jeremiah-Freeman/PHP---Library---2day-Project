5634cba6477baf279dbc5234770228c590857aa7
Dan Lauby and jay freeman <dan@>
2017-03-02 11:07:34 -0800

    update silex routing for author/book twig templates as well as authors/books twig templates

--- app/app.php
+++ app/app.php
@@ -30,6 +30,32 @@
         return $app['twig']->render('authors.html.twig', ['authors' => Author::getAll()]);
     });
 
-    
+    $app->get('/authors/{id}', function($id) use ($app) {
+        $find_author = Author::find($id);
+        return $app['twig']->render('author.html.twig', ['found_author' => $find_author, 'books' => $find_author->getBooks()]);
+    });
+
+    $app->get("/books" , function() use ($app) {
+        return $app ['twig'] -> render('books.html.twig', ['books' => Book::getAll()]);
+    });
+
+    $app->get("/books/{id}", function($id) use ($app) {
+        $find_book = Book::find($id);
+        return $app['twig'] -> render('book.html.twig' , ['found_book' => $find_book, 'authors' => $find_book->getAuthors()]);
+    });
+
+    $app->post("/create/authors", function() use ($app) {
+        $new_author = new Author($_POST['author_name']);
+        $new_author->save();
+        return $app ['twig'] -> render ('authors.html.twig' , ['authors' => Author::getAll()]);
+    });
+
+    $app->post("/create/books", function() use ($app) {
+        $new_book = new Book($_POST['title']);
+        $new_book->save();
+        return $app ['twig'] -> render ('books.html.twig' , ['books' => Book::getAll()]);
+    });
+
+
     return $app;
 ?>
